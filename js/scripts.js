808298773fb5f157e5a4b72c0a47345a16ff062c
Irene DeVera <irenneeyyy@gmail.com>
2017-02-15 20:42:26 -0800

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
   $("form#appointment-info").submit(function(event) {
     event.preventDefault();
-
+//Give inputs variable names
   var name = $("#name").val();
   var description = $("#description").val();
   var date = $("#date").val();
   var time = $("#time").val();
-
+//Output of inputs
   $("span.name").text(name);
   $("span.description").text(description);
   $("span.date").text(date);
   $("span.time").text(time);
-
+//Hide form and show details of appointment
   $("#details").show();
   $("form#appointment-info").hide();
   $("h1").hide();
